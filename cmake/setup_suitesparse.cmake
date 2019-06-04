8fb17ecd5da858bc04160c201f4cb19fc9d8535a
Paul le Roux <pleroux0@outlook.com>
2019-06-04 13:18:37 +0200

    feat: Add source files in repository and fix include paths

--- cmake/setup_suitesparse.cmake
+++ cmake/setup_suitesparse.cmake
@@ -1,69 +1,41 @@
 cmake_minimum_required(VERSION 3.11 FATAL_ERROR)
 
-# Fetch SuiteSparse
-include(FetchContent)
-
-set(SUITESPARSE_URL
-    "http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-5.4.0.tar.gz")
-
-fetchcontent_declare(SUITESPARSE URL "${SUITESPARSE_URL}")
-fetchcontent_getproperties(suitesparse)
-
-if(NOT suitesparse_POPULATED)
-  fetchcontent_populate(SUITESPARSE)
-
-  message("SuiteSparse source directory: ${suitesparse_SOURCE_DIR}")
-  message("SuiteSparse binary directory: ${suitesparse_BINARY_DIR}")
-endif()
-
 include(GNUInstallDirs)
 
-# Install target helper
-function(install_suitesparse_target name)
+# Add PIC option
+if(NOT BUILD_SHARED_LIBS)
+  option(WITH_PIC "Build library with PIC" ON)
+endif()
 
+# Install target helper
+function(suitesparse_target name sources)
   string(TOLOWER ${name} lcname)
+
+  add_library(${lcname} ${sources})
+  add_library(suitesparse::${lcname} ALIAS ${lcname})
 
   install(TARGETS ${lcname}
           EXPORT suitesparse-${lcname}-targets
           LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
           ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
           RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
-          INCLUDES
-          DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+          INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 
   install(EXPORT suitesparse-${lcname}-targets
           FILE CMakeSuiteSparse${name}Config.cmake
           NAMESPACE suitesparse::
           DESTINATION share/CMakeSuiteSparse${name}/)
 
-endfunction()
-
-# Setup library
-function(setup_suitesparse_library name sources)
-
-  string(TOLOWER ${name} lcname)
-  string(TOUPPER ${name} ucname)
-  set(DIR "${suitesparse_SOURCE_DIR}/${name}")
-  list(TRANSFORM sources PREPEND "${DIR}/Source/")
-
-  # Add library and it's alias
-  add_library(${lcname} ${sources})
-  add_library(suitesparse::${lcname} ALIAS ${lcname})
-
-  # Change library properties
   set_target_properties(${lcname} PROPERTIES OUTPUT_NAME suitesparse_${lcname})
-  set_target_properties(${lcname} PROPERTIES POSITION_INDEPENDENT_CODE ON)
+  set_target_properties(${lcname} PROPERTIES POSITION_INDEPENDENT_CODE ${WITH_PIC})
 
-  # Set include directories
-  target_include_directories(${lcname}
-                             PUBLIC $<BUILD_INTERFACE:${DIR}/Include>
-                                    $<INSTALL_INTERFACE:include/suitesparse>)
+  # Add include directories to target
+  target_include_directories(
+    ${lcname}
+    PUBLIC $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>
+           $<INSTALL_INTERFACE:include>)
 
-  # Install headers
-  install(DIRECTORY "${DIR}/Include/"
-          DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/suitesparse")
-
-  # Install targets
-  install_suitesparse_target(${name})
-
+  # Install include directory
+  install(DIRECTORY "${PROJECT_SOURCE_DIR}/include"
+          DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/")
 endfunction()
